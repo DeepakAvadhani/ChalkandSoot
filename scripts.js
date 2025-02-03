f8048dc3478aab9e69bf75a96c1f9073f44238d8
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-02-03 16:30:59 +0530

    added cursor

---
+++ scripts.js
@@ -0,0 +1,7 @@
+gsap.set('.cursor',{xPercent:-50,yPercent:-50});
+
+var cur = document.querySelector('.cursor');
+
+window.addEventListener('mousemove',e => {
+    gsap.to(cur,0.2,{x:e.clientX,y:e.clientY});
+});
